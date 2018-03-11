feced5be91f780a0605af6f06c0accbbcaa85535
Jérémy DA TRINDADE <jeremytrindade@gmail.com>
2018-03-11 16:05:58 +0100

    Le serpent commence a bouger (refreshCanvas)

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 window.onload = function(){
-    var canvas = document.createElement('canvas');
-    canvas.width = 900;
-    canvas.height = 600;
-    canvas.style.border = "1px solid";
-    document.body.appendChild(canvas);
-    var ctx = canvas.getContext('2d');
-    ctx.fillStyle = "#ff0000";
-    ctx.fillRect(30,30,100,50)
+    var canvas;
+    var ctx;
+    var delay = 1000; //le temps son exprimé en miliseconde. donc 1000 miliseconde = 1 seconde.
+    var xCoord = 0;
+    var yCoord = 0;
+    
+    init();
+    
+    function init(){
+        canvas = document.createElement('canvas');
+        canvas.width = 900;
+        canvas.height = 600;
+        canvas.style.border = "1px solid";
+        document.body.appendChild(canvas);
+        ctx = canvas.getContext('2d');
+        refreshCanvas();
+
+    }
+    
+    function refreshCanvas(){
+        
+        xCoord +=5;
+        yCoord +=5;
+        //le montant ajouter au Coord determine la vitesse du serpent.
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        ctx.fillStyle = "#ff0000";
+        ctx.fillRect(xCoord,yCoord,100,50);
+        setTimeout(refreshCanvas,delay);
+    }
 }
