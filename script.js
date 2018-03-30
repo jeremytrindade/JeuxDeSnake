e980dbfcef1f2388a6054f0c11bd535fb077c2a9
jeremytrindade <32887783+jeremytrindade@users.noreply.github.com>
2018-03-30 19:30:06 +0100

    et maintenant ça marche

--- script.js
+++ script.js
@@ -1,113 +1,127 @@
-window.onload = function(){
+window.onload = function()
+{
     var canvasWidth = 900;
     var canvasHeight = 600;
     var blockSize = 30;
     var ctx;
-    var delay = 1000; //le temps son exprimé en miliseconde. donc 1000 miliseconde = 1 seconde.
+    var delay = 100;
     var snakee;
-    
+
     init();
-    
-    function init(){
+
+    function init()
+    {
         var canvas = document.createElement('canvas');
-        canvas.width=canvasWidth;
-        canvas.height=canvasHeight;
-        canvas.style.border = "1px solid";
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
+        canvas.style.border = '1px solid';
         document.body.appendChild(canvas);
         ctx = canvas.getContext('2d');
-        snakee = new Snake([[6,4],[5,4],[4,4]], "right");
+        snakee = new Snake( [[6,4], [5,4], [4,4]], 'right');
         refreshCanvas();
-
     }
-    
-    function refreshCanvas(){
-        /*
-        xCoord +=5;
-        yCoord +=5;
-        le montant ajouter au Coord determine la vitesse du serpent.*/
-        ctx.clearRect(0,0,canvasWidth,canvasHeight);
+
+    function refreshCanvas()
+    {
+        ctx.clearRect(0,0,canvasWidth, canvasHeight);
         snakee.advance();
         snakee.draw();
         setTimeout(refreshCanvas,delay);
-    };
-    
-    function drawBlock(ctx, position){
-        var x=position[0]*blockSize;
-        var y= position[1]*blockSize;
-        ctx.fillRect(x , y , blockSize , blockSize);
-    };
-    
-    function Snake (body, direction){
+    }
+
+    function drawBlock(ctx, position)
+    {
+        var x = position[0] * blockSize;
+        var y = position[1] * blockSize;
+        ctx.fillRect(x ,y , blockSize, blockSize);
+    }
+
+    function Snake(body, direction)
+    {
         this.body = body;
         this.direction = direction;
-        this.draw = function(){
+        this.draw = function()
+        {
             ctx.save();
-            ctx.fillStyle = "#ff0000";
-            for(var i=0; i<this.body.length;i++){
+            ctx.fillStyle = '#ff0000';
+            for(var i = 0; i < this.body.length; i++)
+            {
                 drawBlock(ctx, this.body[i]);
             }
+
             ctx.restore();
-        }
-        this.advance = function(){
+        };
+        this.advance= function()
+        {
             var nextPosition = this.body[0].slice();
-            switch(this.direction){
-                case "letf":
-                    nextPosition[0] -=1;
+            switch(this.direction)
+            {
+                case 'left':
+                    nextPosition[0] -= 1;
                     break;
-                case "right":
-                    nextPosition[0] +=1;
+                case 'right':
+                    nextPosition[0] += 1;
                     break;
-                case "down":
-                    nextPosition[1] +=1;
-                    break; 
-                case "up":
-                    nextPosition[1] -=1;
+                case 'down':
+                    nextPosition[1] += 1;
+                    break;
+                case 'up':
+                    nextPosition[1] -= 1;
                     break;
                 default:
-                    throw("Invalid Direction")
+                    throw('Invalid Direction'); 
             }
             this.body.unshift(nextPosition);
             this.body.pop();
-        }
-        this.setDirection = function(newDirection){
+        };
+        this.setDirection = function(newDirection)
+        {
             var allowedDirections;
-            switch(this.direction){
-                case "letf":
-                case "right":
-                    allowedDirections = ["up", "down"];
+            switch(this.direction)
+            {
+                case 'left':
+                case 'right':
+                allowedDirections = ['up', 'down'];
                     break;
-                case "down":
-                case "up":
-                    allowedDirections = ["left", "right"];
+                case 'down':
+                case 'up':
+                allowedDirections = ['left', 'right'];
                     break;
-                default:
-                    throw("Invalid Direction")
+            default:
+                throw('Invalid Direction');     
             }
-            if(allowedDirections.indexOf(newDirection) > -1){
+            if(allowedDirections.indexOf(newDirection) > -1)
+            {
                 this.direction = newDirection;
             }
         };
+        
     }
-        document.onkeydown = function handleKeyDown(e){
-            var key = e.keyCode;
-            var newDirection;
-            switch(key){
-                case 37:
-                    newDirection = "left";
-                    break;
-                case 38:
-                    newDirection = "up";
-                    break;
-                case 39:
-                    newDirection = "right";
-                    break;
-                case 40:
-                    newDirection = "down";
-                    break;
-                default:
-                    return;
-            }
-            snakee.setDirection(newDirection);
+
+    document.onkeydown = function handleKeyDown(e)
+    {
+        var key = e.keyCode;
+        var newDirection;
+        switch(key)
+        {
+            case 37:
+                newDirection = 'left';
+                break;
+            case 38:
+            newDirection = 'up';
+                break;
+            case 39:
+            newDirection = 'right';
+                break;
+            case 40:
+            newDirection = 'down';
+                break;
+            default:
+                return; 
+
+        }
+        snakee.setDirection(newDirection);
+
     }
-    
+
 }
