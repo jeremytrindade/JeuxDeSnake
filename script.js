4ae09ec952bd0ec918f290e09fe5cb6094041bb5
Jérémy DA TRINDADE <jeremytrindade@gmail.com>
2018-03-30 19:24:52 +0100

    maintenant il suis les fleche de l'ordinateur, mais il ne vas pas a gauche.

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ window.onload = function(){
         canvas.style.border = "1px solid";
         document.body.appendChild(canvas);
         ctx = canvas.getContext('2d');
-        snakee = new Snake([[6,4],[5,4],[4,4]]);
+        snakee = new Snake([[6,4],[5,4],[4,4]], "right");
         refreshCanvas();
 
     }
@@ -29,16 +29,17 @@ window.onload = function(){
         snakee.advance();
         snakee.draw();
         setTimeout(refreshCanvas,delay);
-    }
+    };
     
     function drawBlock(ctx, position){
         var x=position[0]*blockSize;
         var y= position[1]*blockSize;
         ctx.fillRect(x , y , blockSize , blockSize);
-    }
+    };
     
-    function Snake (body){
+    function Snake (body, direction){
         this.body = body;
+        this.direction = direction;
         this.draw = function(){
             ctx.save();
             ctx.fillStyle = "#ff0000";
@@ -49,9 +50,64 @@ window.onload = function(){
         }
         this.advance = function(){
             var nextPosition = this.body[0].slice();
-            nextPosition[0] +=1;
+            switch(this.direction){
+                case "letf":
+                    nextPosition[0] -=1;
+                    break;
+                case "right":
+                    nextPosition[0] +=1;
+                    break;
+                case "down":
+                    nextPosition[1] +=1;
+                    break; 
+                case "up":
+                    nextPosition[1] -=1;
+                    break;
+                default:
+                    throw("Invalid Direction")
+            }
             this.body.unshift(nextPosition);
             this.body.pop();
         }
+        this.setDirection = function(newDirection){
+            var allowedDirections;
+            switch(this.direction){
+                case "letf":
+                case "right":
+                    allowedDirections = ["up", "down"];
+                    break;
+                case "down":
+                case "up":
+                    allowedDirections = ["left", "right"];
+                    break;
+                default:
+                    throw("Invalid Direction")
+            }
+            if(allowedDirections.indexOf(newDirection) > -1){
+                this.direction = newDirection;
+            }
+        };
     }
+        document.onkeydown = function handleKeyDown(e){
+            var key = e.keyCode;
+            var newDirection;
+            switch(key){
+                case 37:
+                    newDirection = "left";
+                    break;
+                case 38:
+                    newDirection = "up";
+                    break;
+                case 39:
+                    newDirection = "right";
+                    break;
+                case 40:
+                    newDirection = "down";
+                    break;
+                default:
+                    return;
+            }
+            snakee.setDirection(newDirection);
+    }
+    
 }
