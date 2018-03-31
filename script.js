6908065b42ea3317ebe82d427ca37f12fcdd23a9
Jérémy DA TRINDADE <jeremytrindade@gmail.com>
2018-03-31 10:18:32 +0100

    Score et Game Over

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ window.onload = function()
     var applee;
     var widthInBlocks = canvasWidth/blockSize;
     var heightInBlocks = canvasHeight/blockSize;
+    var score;
 
     init();
 
@@ -22,6 +23,7 @@ window.onload = function()
         ctx = canvas.getContext('2d');
         snakee = new Snake( [[6,4], [5,4], [4,4]], 'right');
         applee = new Apple([10,10]);
+        score = 0;
         refreshCanvas();
     }
 
@@ -30,12 +32,13 @@ window.onload = function()
         snakee.advance();
         if(snakee.checkCollision())
         {
-            // GAME OVER
+            gameOver();
         }
         else
         {
             if(snakee.isEatingApple(applee))
             {
+                score++;
                 snakee.ateApple = true;
                 do
                 {
@@ -47,9 +50,31 @@ window.onload = function()
             ctx.clearRect(0,0,canvasWidth, canvasHeight);
             snakee.draw();
             applee.draw();
+            drawScore();
             setTimeout(refreshCanvas,delay);
         }
         
+    }
+    function gameOver()
+    {
+        ctx.save();
+        ctx.fillText("Game Over", 5, 15)
+        ctx.fillText("Appuyer sur la touche Espace pour rejouer", 5, 30);
+
+        ctx.restore();
+    }
+    function restart()
+    {
+        snakee = new Snake( [[6,4], [5,4], [4,4]], 'right');
+        applee = new Apple([10,10]);
+        score = 0;
+        refreshCanvas();
+    }
+    function drawScore()
+    {
+        ctx.save();
+        ctx.fillText(score.toString(), 5, canvasHeight - 5);
+        ctx.restore();
     }
 
     function drawBlock(ctx, position)
@@ -218,6 +243,9 @@ window.onload = function()
             case 40:
             newDirection = 'down';
                 break;
+            case 32:
+                restart();
+                return;
             default:
                 return; 
 
